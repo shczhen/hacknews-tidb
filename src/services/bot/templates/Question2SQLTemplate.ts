c81b488c87628811f06fa31b2c3f7a1ea5ef26cd
sykp241095 <sykp241095@gmail.com>
2023-01-23 21:14:38 +0800

    Update Question2SQLTemplate.ts

--- src/services/bot/templates/Question2SQLTemplate.ts
+++ src/services/bot/templates/Question2SQLTemplate.ts
@@ -38,7 +38,7 @@ class Question2SQLTemplate implements Template {
 # Notice: don't output superfluous and leading double quote in sql
 
 Must append the limit to SQL!!!
-Let's think step by step, use best practice of writing SQL, use common table expression if and only if necessary, generate only one SQL to answer this question: "(I want you think like a god)${question}"`;
+Let's think step by step, use best practice of writing SQL, use common table expression if necessary, generate only one SQL to answer this question: "(I want you think like a god)${question}"`;
   }
 }
 
