98ef5c081b689d4bb20e564b099c8b831bf02bf3
czhen <56986964+shczhen@users.noreply.github.com>
2023-02-07 14:21:22 +0800

    refine retry button

--- src/components/Button/RetryButton.tsx
+++ src/components/Button/RetryButton.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
 import ReplayIcon from '@mui/icons-material/Replay';
 
 export interface RetryButtonProps {
-  onClick?: () => void;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export default function RetryButton(props: RetryButtonProps) {
@@ -25,5 +26,27 @@ export default function RetryButton(props: RetryButtonProps) {
     >
       Retry
     </Button>
+  );
+}
+
+export function RetryIconButton(props: RetryButtonProps) {
+  const { onClick } = props;
+
+  return (
+    <IconButton
+      aria-label="retry"
+      onClick={onClick}
+      size="small"
+      sx={{
+        color: 'white',
+        fontSize: '0.75rem',
+        backgroundColor: 'hn.primary',
+        '&:hover': {
+          backgroundColor: 'hn.primary',
+        },
+      }}
+    >
+      <ReplayIcon fontSize="inherit" />
+    </IconButton>
   );
 }
